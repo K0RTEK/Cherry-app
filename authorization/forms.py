d5c09b23b06e735bb00954fb1c230c9dd746cb95
Kirill <grias0001@gmail.com>
2023-11-27 23:47:07 +0300

    test

--- authorization/forms.py
+++ authorization/forms.py
@@ -1,5 +1,15 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
+
 from .models import AudioFile
+
+
+# class CustomAuthenticationForm(AuthenticationForm):
+#     def __init__(self, *args, **kwargs):
+#         super(CustomAuthenticationForm, self).__init__(*args, **kwargs)
+#         self.fields['username'].widget.attrs['class'] = "form-control"
+#         self.fields['password'].widget.attrs['class'] = "form-control"
 
 
 class AudioUploadForm(forms.ModelForm):
--- authorization/views.py
+++ authorization/views.py
@@ -4,8 +4,19 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from pydub import AudioSegment
+
 from .forms import AudioUploadForm
 from .models import AudioFile
+
+import os
+import wave
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import json
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
 def login_page(request):
@@ -29,8 +40,58 @@ def login_page(request):
 
 
 def process_audio(audio_file):
+    # Установите уровень логирования Vosk (опционально)
+    SetLogLevel(0)
+
+    # Загрузите модель для русского языка
+    model = Model("authorization/vosk-model-small-ru-0.22")
+    mp3_audio = AudioSegment.from_mp3(audio_file.name)
+    audio_file = mp3_audio.export(audio_file.name.replace(".mp3", ".wav"), format="wav")
+
+    # Откройте аудиофайл
+    wf = wave.open(audio_file, "rb")
+
+    # Создайте распознаватель
+    rec = KaldiRecognizer(model, wf.getframerate())
+
+    # Список для хранения результатов распознавания
+    results = []
+
+    # Читайте данные из аудиофайла и передавайте их распознавателю
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            result = json.loads(rec.Result())
+            results.append(result)
+    text = ''
+    for elem in results:
+        text += list(elem.values())[0] + ' '
+
+    model = load_model("my_model.h5")
+
+    max_words = 10000  # Максимальное количество слов в словаре
+    max_len = 100  # Максимальная длина последовательности
+
+    tokenizer = Tokenizer(num_words=max_words, oov_token='<OOV>')
+    tokenizer.fit_on_texts([text])
+    sequences = tokenizer.texts_to_sequences([text])
+    padded_sequence = pad_sequences(sequences, maxlen=max_len, padding='post', truncating='post')
+
+    # Выполнение предсказания
+    predictions = model.predict(padded_sequence)
+
+    # Вывод результатов
+
+    class_names = ['плохой', 'хороший']
+
+    predicted_class_index = tf.argmax(predictions, axis=-1).numpy()[0]
+    predicted_class = class_names[predicted_class_index]
+
+    print("Предсказанный класс:", predicted_class)
     # Обработка аудио файла
-    result = "Результат обработки аудио файла"
+    result = predicted_class
     return result
 
 
